70f3a8728a9c59c6ba98a6990517f75316ff211c
Jordi Condom Tibau <jordi.condom@gmail.com>
2020-04-04 19:32:28 +0200

    :)
    Adding all window classes

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from mainWindow import MainWindow
+from windowsFolder.nom import Nom
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    mainWindow = MainWindow()
+    mainWindow = Nom()
     mainWindow.show()
 
     sys.exit(app.exec_())
---
+++ windowsFolder/examen3.py
@@ -0,0 +1,19 @@
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+
+w = 1000
+h = 600
+
+class Examen3(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setGeometry(0,0,w,h)
+
+        textLabel = QLabel(self)
+        textLabel.setText("Examen3")
+        textLabel.setWordWrap(True)
+        textLabel.move(0.4*w, h/3)
+        textLabel.setAlignment(Qt.AlignCenter)
