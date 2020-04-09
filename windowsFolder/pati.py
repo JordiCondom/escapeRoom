adde5e5e429aae2a7682150e5f9bb46fa7c49b81
rafahajjar <rafahajjar@gmail.com>
2020-04-09 21:38:42 +0200

    pati començat

--- windowsFolder/pati.py
+++ windowsFolder/pati.py
@@ -2,6 +2,7 @@ import sys
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from windowsFolder.senyorGrane import SenyorGrane
 
 w = 900
 h = 600
@@ -11,13 +12,47 @@ class Pati(QWidget):
         super().__init__()
 
         self.setGeometry(0,0,w,h)
+        self.backgroundImage = QLabel(self)
+        self.textBubble = QLabel(self)
+        self.previousButton = QPushButton(self)
+        self.nextButton = QPushButton(self)
+        self.textField = QLabel(self)
         self.initWindow()
 
         self.show()
 
     def initWindow(self):
-        self.backgroundImage = QLabel(self)
         pixmap = QPixmap("./images/pati1.jpeg").scaled(w,h)
         self.backgroundImage.setPixmap(pixmap)
+
+        self.previousButton.move(0.12*w, 0.83*h)
+        self.previousButton.setIcon(QIcon('./images/icons/previous.png'))
+        self.previousButton.setStyleSheet("QPushButton{background: transparent;}")
+        self.previousButton.hide()
+
+        bubblePM = QPixmap("./images/icons/textBubble.png")
+        self.textBubble.setPixmap(bubblePM)
+        self.textBubble.move(0.1*w, 0.75*h)
+
+        self.textField.setText("Tota aquesta gent farà NP? Sembla una festa de dia")
+        self.textField.setAlignment(Qt.AlignJustify)
+        self.textField.setWordWrap(True)
+        self.textField.resize(600, 200)
+        self.textField.move(0.167*w, 0.8*h)
+
+        self.nextButton.move(0.85*w,0.83*h)
+        self.nextButton.setIcon(QIcon('./images/icons/next.png'))
+        self.nextButton.setStyleSheet("QPushButton{background: transparent;}")
+        try:
+            self.nextButton.clicked.disconnect()
+        except:
+            pass
+        self.nextButton.clicked.connect(self.senyorGrane)
+
+    def senyorGrane(self):
+        self.senyorGrane = SenyorGrane()
+        self.close()
+
+
 
         
