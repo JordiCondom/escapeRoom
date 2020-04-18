019460ecdaf35d2a6736d88b2c8b15cfc2805516
rafahajjar <rafahajjar@gmail.com>
2020-04-18 13:56:25 +0200

    notes

--- windowsFolder/examen1.py
+++ windowsFolder/examen1.py
@@ -17,12 +17,13 @@ class Examen1(QWidget):
         self.nextButton = QPushButton(self)
         self.textField = QLabel(self)
         self.checkButton = QPushButton('Comprova', self)
+        self.noteInput = NoteInput()
         self.initWindow()
 
         self.show()
 
     def initWindow(self):
-        pixmap = QPixmap("./images/pati/pati1.jpeg").scaled(w,h)
+        pixmap = QPixmap("./images/examen1/padro.JPG").scaled(w,h)
         self.backgroundImage.setPixmap(pixmap)
 
         self.previousButton.move(0.12*w, 0.83*h)
@@ -34,8 +35,7 @@ class Examen1(QWidget):
         self.textBubble.setPixmap(bubblePM)
         self.textBubble.move(0.1*w, 0.75*h)
 
-        self.textField.setText('''<p>- Què esta passant, què fa tanta gent al pati? Perdona, que m'he perdut alguna cosa? Què és tota aquesta gent? <br>
-        <b>Estudiant anònim de 8è: </b> No home no, tranquil, és que avui hi ha examen d'EDE's i hem quedat per fer NP i celebrar-ho. Apuntat-hi, a les 9:00 farem un comunicat i a les 9:30 toquen lectures de finals d'altres anys.<p>''')
+        self.textField.setText('''-Examen de numèrica yuhuuuuu + Sisi, crec que aquest any ho hem acoseguit i hem posat un examen de 100% numèrica. ''')
         self.textField.setAlignment(Qt.AlignJustify)
         self.textField.setWordWrap(True)
         self.textField.resize(600, 200)
@@ -48,14 +48,108 @@ class Examen1(QWidget):
             self.nextButton.clicked.disconnect()
         except:
             pass
-        self.nextButton.clicked.connect(self.covit)
+        self.nextButton.clicked.connect(self.criba)
 
-        self.checkButton.move(0.75*w, 0.83*h)
+        self.checkButton.move(0.8*w, 0.86*h)
         self.checkButton.clicked.connect(self.checkAnswer)
         self.checkButton.hide()
 
-    def covit(self):
-        pass
+        self.noteInput.move(0.45*w, 0.75*h)
+        self.noteInput.setParent(self)
+        self.noteInput.hide()
+
+    def criba(self):
+        pixmap = QPixmap("./images/examen1/partitura.png").scaled(w,h)
+        self.backgroundImage.setPixmap(pixmap)
+
+        self.textBubble.hide()
+        self.textField.hide()
+        self.nextButton.hide()
+
+        self.noteInput.show()
+        self.checkButton.show()
 
     def checkAnswer(self):
-        pass
+        n1 = self.noteInput.note1.notes.currentIndex()
+        n2 = self.noteInput.note2.notes.currentIndex()
+        n3 = self.noteInput.note3.notes.currentIndex()
+        n4 = self.noteInput.note4.notes.currentIndex()
+        if n1 == 0 and n2 == 1 and n3 == 3 and n4 == 8:
+            self.solved()
+
+    def solved(self):
+        pixmap = QPixmap("./images/examen1/padro.JPG").scaled(w,h)
+        self.backgroundImage.setPixmap(pixmap)
+
+        self.hide()
+        self.show()
+
+class NoteInput(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QHBoxLayout()
+
+        self.note1 = NoteSelector()
+        self.note2 = NoteSelector()
+        self.note3 = NoteSelector()
+        self.note4 = NoteSelector()
+
+        layout.addWidget(self.note1)
+        layout.addWidget(self.note2)
+        layout.addWidget(self.note3)
+        layout.addWidget(self.note4)
+
+        layout.setSpacing(0)
+
+        self.setLayout(layout)
+
+
+class NoteSelector(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        layout = QVBoxLayout()
+
+        up = QPushButton()
+        up.setIcon(QIcon('./images/icons/up.png'))
+        up.setStyleSheet("QPushButton{background: transparent;}")
+        up.clicked.connect(self.upgrade)
+
+        down = QPushButton()
+        down.setIcon(QIcon('./images/icons/down.png'))
+        down.setStyleSheet("QPushButton{background: transparent;}")
+        down.clicked.connect(self.downgrade)
+
+        self.notes = QStackedWidget()
+
+        for i in range(1,14):
+            note = QLabel()
+            note.setPixmap(QPixmap("./images/notes/" + str(i) + ".png"))
+            self.notes.addWidget(note)
+
+        layout.addWidget(up)
+        layout.addWidget(self.notes)
+        layout.addWidget(down)
+
+        layout.setSpacing(0)
+
+        self.setLayout(layout)
+
+    def upgrade(self):
+        n = self.notes.count()
+        index = self.notes.currentIndex()
+        newIndex = index + 1
+        if newIndex == n: newIndex = 0
+
+        self.notes.setCurrentIndex(newIndex)
+
+    def downgrade(self):
+        n = self.notes.count()
+        index = self.notes.currentIndex()
+        newIndex = index - 1
+        if newIndex == -1: newIndex = n-1
+
+        self.notes.setCurrentIndex(newIndex)
+
+
